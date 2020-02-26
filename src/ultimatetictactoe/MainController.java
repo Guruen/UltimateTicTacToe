0655c0d6801aa70005408ed4a84e0650a545fd79
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-02-26 09:56:12 +0100

    yeah

--- src/ultimatetictactoe/MainController.java
+++ src/ultimatetictactoe/MainController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import ultimatetictactoe.game.GameManager;
 import ultimatetictactoe.game.GameState;
 import ultimatetictactoe.game.IGameState;
@@ -24,24 +25,36 @@ public class MainController implements Initializable {
 
     private GameManager gm;
     IGameState gameState;
+    @FXML
+    private AnchorPane AnchorPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gameState = new GameState();
         gm = new GameManager(gameState);
+
+        UTTTButton btn = new UTTTButton();
+        btn.setPrefSize(30, 30);
+        btn.setMove(new Move(0, 0));
+        btn.setLayoutX(10);
+        btn.setLayoutY(10);
+        btn.setOnMouseClicked(event -> {
+            UTTTButton b = (UTTTButton) event.getSource();
+            boolean moveSucces = gm.updateGame(btn.getMove());
+            if (moveSucces) {
+                if (gameState.getMoveNumber() % 2 == 0) {
+                    b.setText("X");
+                } else {
+                    b.setText("O");
+                }
+            }
+        });
+        AnchorPane.getChildren().add(btn);
     }
 
     @FXML
     private void clickCell(ActionEvent event) {
-        JFXButton btn = (JFXButton) event.getSource();
-        boolean moveSucces = gm.updateGame(new Move(0, 0));
-        if (moveSucces) {
-            if (gameState.getMoveNumber() % 2 == 0) {
-                btn.setText("X");
-            } else {
-                btn.setText("O");
-            }
-        }
+
         //btn.setStyle("-fx-background-color: #0000FF; -fx-border-color: #000000");
     }
 
