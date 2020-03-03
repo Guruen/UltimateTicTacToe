685512a06dd90ec43c36b2d8de35ef05178507ff
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-03-03 11:39:46 +0100

    Shadows working

--- src/ultimatetictactoe/MainController.java
+++ src/ultimatetictactoe/MainController.java
@@ -6,48 +6,57 @@
 package ultimatetictactoe;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import ultimatetictactoe.bot.Bot;
 import ultimatetictactoe.bot.IBot;
 import ultimatetictactoe.game.GameManager;
 import ultimatetictactoe.game.GameState;
 import ultimatetictactoe.game.IGameState;
+import ultimatetictactoe.move.IMove;
 import ultimatetictactoe.move.Move;
 
 /**
  *
  * @author Brian
  */
-public class MainController implements Initializable {
+public class MainController implements Initializable
+{
 
     private GameManager gm;
     IGameState gameState;
     IBot bot;
     @FXML
     private AnchorPane AnchorPane;
+    private List<IMove> removeShadowsList;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
 
         gameState = new GameState();
         bot = new Bot();
         gm = new GameManager(gameState, bot);
         creatAllCells();
-
+        
     }
 
-    private void creatAllCells() {
+    private void creatAllCells()
+    {
         int btnWidth = 30;
         int btnHeight = 30;
         int boardOffsetEdge = 20;
         int macroBoardPadding = 8;
 
-        for (int x = 0; x < 9; x++) {
-            for (int y = 0; y < 9; y++) {
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
                 String sX = x + "";
                 String sY = y + "";
                 UTTTButton btn = new UTTTButton();
@@ -55,8 +64,8 @@ public class MainController implements Initializable {
                 btn.setMove(new Move(x, y));
                 btn.setId(sX + sY);
 
-                int paddingX = boardOffsetEdge + (x/3 )* macroBoardPadding;
-                int paddingY = boardOffsetEdge + (y/3) * macroBoardPadding;
+                int paddingX = boardOffsetEdge + (x / 3) * macroBoardPadding;
+                int paddingY = boardOffsetEdge + (y / 3) * macroBoardPadding;
 
                 btn.setLayoutX(paddingX + btnWidth * x);
                 btn.setLayoutY(paddingY + btnHeight * y);
@@ -64,13 +73,18 @@ public class MainController implements Initializable {
                 {
                     UTTTButton b = (UTTTButton) event.getSource();
                     boolean moveSucces = gm.updateGame(b.getMove());
-                    if (moveSucces) {
-                        if (gameState.getMoveNumber() % 2 == 0) {
+                    if (moveSucces)
+                    {
+                        if (gameState.getMoveNumber() % 2 == 0)
+                        {
                             b.setText("X");
-                        } else {
+                        } else
+                        {
                             b.setText("O");
                         }
                         botMove();
+                        removeShadows();
+                        setShadowsOnButtons();
                     }
                 });
                 AnchorPane.getChildren().add(btn);
@@ -79,7 +93,8 @@ public class MainController implements Initializable {
 
     }
 
-    private void botMove() {
+    private void botMove()
+    {
         gm.updateGame();
 
         String x = Integer.toString(gm.getBotMove().getX());
@@ -89,17 +104,63 @@ public class MainController implements Initializable {
 
         UTTTButton foundBtn = (UTTTButton) AnchorPane.lookup(findBtn);
 
-        if (gameState.getMoveNumber() % 2 == 0) {
+        if (gameState.getMoveNumber() % 2 == 0)
+        {
             foundBtn.setText("X");
-        } else {
+        } else
+        {
             foundBtn.setText("O");
         }
 
     }
 
+    private void setShadowsOnButtons()
+    {
+        DropShadow shadow = new DropShadow();
+        List<IMove> buttonsToShow = gameState.getField().getAvailableMoves();
+        removeShadowsList = buttonsToShow;
+
+        for (int i = 0; i < buttonsToShow.size(); i++)
+        {
+            String x = Integer.toString(buttonsToShow.get(i).getX());
+            String y = Integer.toString(buttonsToShow.get(i).getY());
+
+            String findBtn = "#" + x + y;
+
+            UTTTButton foundBtn = (UTTTButton) AnchorPane.lookup(findBtn);
+
+            foundBtn.setEffect(shadow);
+        }
+
+    }
+
+    private void removeShadows()
+    {
+        try
+        {
+            if (!removeShadowsList.isEmpty())
+            {
+                for (int i = 0; i < removeShadowsList.size(); i++)
+                {
+                    String x = Integer.toString(removeShadowsList.get(i).getX());
+                    String y = Integer.toString(removeShadowsList.get(i).getY());
+
+                    String findBtn = "#" + x + y;
+
+                    UTTTButton foundBtn = (UTTTButton) AnchorPane.lookup(findBtn);
+
+                    foundBtn.setEffect(null);
+                }
+            }
+        } catch (Exception e)
+        {
+        }
+
+    }
 
     @FXML
-    private void clickCell(ActionEvent event) {
+    private void clickCell(ActionEvent event)
+    {
 
         //btn.setStyle("-fx-background-color: #0000FF; -fx-border-color: #000000");
     }
