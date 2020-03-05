5ba1b09a5b19267f74913507fb5cebc4f56a96c3
Guruen <56912822+Guruen@users.noreply.github.com>
2020-03-05 08:35:16 +0100

    Update mySpot.java

--- src/ultimatetictactoe/bot/mySpot.java
+++ src/ultimatetictactoe/bot/mySpot.java
@@ -16,44 +16,142 @@ import ultimatetictactoe.move.Move;
  *
  * @author BBran
  */
-public class mySpot implements IBot {
+public class mySpot implements IBot
+{
 
     private static final String BOTNAME = "Gimme That Spot!";
     private String[][] localBoard = new String[9][9];
-    private int plusIndex;
-    private int minusIndex;
+    private int x1;
+    private int x2;
+    private int x3;
+    private int y1;
+    private int y2;
+    private int y3;
     private String playerID;
 
     @Override
-    public IMove doMove(IGameState state) {
-
-        if (state.getMoveNumber() % 2 == 0) {
+    public IMove doMove(IGameState state)
+    {
+        
+        if (state.getMoveNumber() % 2 == 0)
+        {
             playerID = "" + 0;
-        } else {
+        } else
+        {
             playerID = "" + 1;
         }
 
         localBoard = state.getField().getBoard();
 
-        for (int x = 0; x < localBoard.length; x++) {
-            for (int y = 0; y < localBoard.length; y++) {
-                if (state.getField().isInActiveMicroboard(x, y)) {
+        for (int x = 0; x < localBoard.length; x++)
+        {
+            for (int y = 0; y < localBoard.length; y++)
+            {
+                if (state.getField().isInActiveMicroboard(x, y))
+                {
 
-                    if (x == 2 || x == 5 || x == 8)
+                    if (x >= 6)
                     {
-                        plusIndex = 0;
+                        x1 = 6;
+                        x2 = 7;
+                        x3 = 8;
+                    } else if (x >= 3)
+                    {
+                        x1 = 3;
+                        x2 = 4;
+                        x3 = 5;
+                    } else
+                    {
+                        x1 = 0;
+                        x2 = 1;
+                        x3 = 2;
                     }
-                    else
+
+                    if (y >= 6)
                     {
-                        plusIndex = 1;
+                        y1 = 6;
+                        y2 = 7;
+                        y3 = 8;
+                    } else if (x >= 3)
+                    {
+                        y1 = 3;
+                        y2 = 4;
+                        y3 = 5;
+                    } else
+                    {
+                        y1 = 0;
+                        y2 = 1;
+                        y3 = 2;
+                    }
+                    //Check Rows for valid spot
+                    if (localBoard[x1][y].equals(playerID) && localBoard[x2][y].equals(playerID) && localBoard[x3][y].equals(IField.EMPTY_FIELD))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x1 + "," + y + " & " + x2 + "," + y);
+                        if (localBoard[x3][y].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x3 + "," + y);
+                            IMove m = new Move(x3, y);
+                            return m;
+                        }
+                    }
+
+                    if (localBoard[x1][y].equals(IField.EMPTY_FIELD) && localBoard[x2][y].equals(playerID) && localBoard[x3][y].equals(playerID))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x2 + "," + y + " & " + x3 + "," + y);
+                        if (localBoard[x1][y].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x1 + "," + y);
+                            IMove m = new Move(x1, y);
+                            return m;
+                        }
+                    }
+
+                    if (localBoard[x1][y].equals(playerID) && localBoard[x2][y].equals(IField.EMPTY_FIELD) && localBoard[x3][y].equals(playerID))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x1 + "," + y + " & " + x3 + "," + y);
+                        if (localBoard[x2][y].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x2 + "," + y);
+                            IMove m = new Move(x2, y);
+                            return m;
+                        }
+                    }
+
+
+                    // checks cols for valid move
+                    if (localBoard[x][y1].equals(playerID) && localBoard[x][y2].equals(playerID) && localBoard[x][y3].equals(IField.EMPTY_FIELD))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x + "," + y1 + " & " + x + "," + y2);
+                        if (localBoard[x][y3].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x + "," + y3);
+                            IMove m = new Move(x, y3);
+                            return m;
+                        }
+                    }
+                    if (localBoard[x][y1].equals(IField.EMPTY_FIELD) && localBoard[x][y2].equals(playerID) && localBoard[x][y3].equals(playerID))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x + "," + y2 + " & " + x + "," + y3);
+                        if (localBoard[x][y1].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x + "," + y1);
+                            IMove m = new Move(x, y1);
+                            return m;
+                        }
+                    }
+
+                    if (localBoard[x][y1].equals(playerID) && localBoard[x][y2].equals(IField.EMPTY_FIELD) && localBoard[x][y3].equals(playerID))
+                    {
+                        System.out.println("Found playerID: " + playerID + " in coords: " + x + "," + y1 + " & " + x + "," + y3);
+                        if (localBoard[x][y2].equals(IField.EMPTY_FIELD))
+                        {
+                            System.out.println("Play Valid move: " + x + "," + y2);
+                            IMove m = new Move(x, y2);
+                            return m;
+                        }
                     }
                     
-                    //Check Rows for playerid
-                    if (localBoard[x][y].equals(playerID))
-                    {
-                        int mN = x+plusIndex;
-                        System.out.println("Found playerID: " + playerID + " in coords: " + x + "," + y + " and " + mN + "," + y);
-                    }
+                    
                     
                     
                     System.out.println("cordinate is: " + x + "," + y + " contains: " + localBoard[x][y]);
@@ -71,7 +169,8 @@ public class mySpot implements IBot {
     }
 
     @Override
-    public String getBotName() {
+    public String getBotName()
+    {
         return BOTNAME;
     }
 
